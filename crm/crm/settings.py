aa8773331c39e2134a457fddddbcf42f52f50789
jacobkbny <pkg1028@gmail.com>
2023-07-30 22:46:02 +0900

    contettypes added

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -35,7 +35,7 @@ INSTALLED_APPS = [
     # 'django.contrib.admin',
     'django.contrib.staticfiles',
     # 'django.contrib.auth',
-    # 'django.contrib.contenttypes',
+    'django.contrib.contenttypes',
     # 'django.contrib.sessions',
     # 'django.contrib.messages',
     "email_sender",
